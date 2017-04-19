392fc836c888b18c16487b8c0cfbabe500a0a4c7
Lukasz Kolacz <lukaszkolacz15@gmail.com>
2017-04-19 17:10:02 +0200

    PASWORD REGEX

--- src/pl/akademiakodu/Main.java
+++ src/pl/akademiakodu/Main.java
@@ -1,7 +1,9 @@
 package pl.akademiakodu;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 public class Main {
 
@@ -12,8 +14,6 @@ public class Main {
 //        Pattern pattern = Pattern.compile("\\S\\D{1,}");
 //        Pattern pattern = Pattern.compile("\\d{1,9}");
 //        Pattern pattern = Pattern.compile("(\\p{Upper}){1,}\\d{1,}"); ????????????
-
-
 
 
 //        Matcher matcher = pattern.matcher("");
@@ -33,14 +33,15 @@ public class Main {
 // 5.
 //        System.out.println(pattern.matcher("").matches());
 
-        System.out.println(Pattern.matches("[789]\\d{0,8}", "7"));
+//        System.out.println(Pattern.matches("[789]\\d{0,8}", "7"));
+//
+//        System.out.println(Pattern.matches("[A-Z].+\\s[A-Z].+", "Oskar Polak"));
+//        System.out.println(Pattern.matches("\\d.*\\D", "5asdasdD"));
+//        System.out.println(Pattern.matches("\\d.*[a-zA-z]", "5asdasdD"));
+//
+//        System.out.println(Pattern.matches(".+@.+\\..+", "lukaszkolacz@gmail.com"));
 
-        System.out.println(Pattern.matches("[A-Z].+\\s[A-Z].+", "Oskar Polak"));
-        System.out.println(Pattern.matches("\\d.*\\D", "5asdasdD"));
-        System.out.println(Pattern.matches("\\d.*[a-zA-z]", "5asdasdD"));
-
-        System.out.println(Pattern.matches(".+@.+\\..+", "lukaszkolacz@gmail.com"));
-
+//        System.out.println(Pattern.matches("\\+48[0-9]{3}-[0-9]{3}-[0-9]{3}", "+48600-234-656"));
 
 
         //Hasło z różnymi znakami
@@ -50,15 +51,68 @@ public class Main {
 //        Pattern intchar = Pattern.compile(".*[\\d].*");
 //
 //        Matcher matcherBig = bigChars.matcher("Aasd48as%dsd");
-//        Matcher matcherSpecial =specjalChars.matcher("Aasd48as%dsd");
+//        Matcher matcherSpecial = specjalChars.matcher("Aasd48as%dsd");
 //        Matcher matcherInt = intchar.matcher("Aasd48as%dsd");
-
+//
+//
 //        if(matcherBig.matches() && matcherSpecial.matches() && matcherInt.matches()){
 //            System.out.println("Hasło jest poprawne");
 //        }
 
 
+//          Metoda sprawdzająca poprawną walidację hasła,
+//          wykonuje się do puki nie wprowadzimy hasla:
+//          skladajacego sie z duzysch, malych liter, cyfr oraz znakow specjalnych np: Aasd48as%dsd
 
+//        passwordRegex();
+//        "\\+48( ?([0-9]{3}-[0-9]{3}-[0-9]{3}))"
+//        System.out.println(Pattern.matches("\\+48( ?[0-9]{3}-[0-9]{3}-[0-9]{3})", "+48-215-587-654"));
+//        System.out.println(Pattern.matches(".+ ?.+", "Łukasz Kołacz"));
+//        System.out.println(Pattern.matches(".{9}", "123 123 123"));
 
+        Scanner scanner = new Scanner(System.in);
+        Boolean status;
+        do {
+
+            String telephoneNumber = scanner.next();
+
+            Pattern tele = Pattern.compile(".{9}");
+            Matcher telephone = tele.matcher(telephoneNumber);
+
+            if (!telephone.matches()) {
+                System.out.println("<!> NUMER TELEFONU MUSI BYĆ JEDNYM CIĄGIEM ZNAKÓW <!>");
+                status = true;
+            }else {
+                System.out.println("poprawne");
+                status = false;
+            }
+
+        }while (status == true);
+
+        System.out.println("Poszło dalej");
+    }
+
+    private static void passwordRegex() {
+        Boolean answer;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            String password = scanner.next();
+
+            Pattern bigChars = Pattern.compile(".*[A-Z].*");
+            Pattern specjalChars = Pattern.compile(".*[\\W].*");
+            Pattern intchar = Pattern.compile(".*[\\d].*");
+
+            Matcher matcherBig = bigChars.matcher(password);
+            Matcher matcherSpecial = specjalChars.matcher(password);
+            Matcher matcherInt = intchar.matcher(password);
+
+            if (!(matcherBig.matches() && matcherSpecial.matches() && matcherInt.matches())) {
+                System.out.println("<!> Hasło niepoprawne! <!>");
+                answer = true;
+            } else {
+                System.out.println("Hasło poprawne!");
+                answer = false;
+            }
+        } while (answer == true);
     }
 }
